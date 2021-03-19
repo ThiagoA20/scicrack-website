904fcb8110e2823f5007791ecddbee69ca717f75
ThiagoA20 <67110486+ThiagoA20@users.noreply.github.com>
2021-03-19 01:13:56 -0300

    Update settings.py

--- ecosci/settings.py
+++ ecosci/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kh@qfrixx-q8gk@+@y)&3kh5)sqe9l+j)0&s-du=18a6@bo!wj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
